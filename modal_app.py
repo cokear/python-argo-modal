adb4e21c465fe9d5606388cfab4db8e433743267
cokear <138343973+cokear@users.noreply.github.com>
2026-06-04 20:43:27 +0800

    Update modal_app.py

--- modal_app.py
+++ modal_app.py
@@ -4,7 +4,6 @@ import modal
 
 app = modal.App("data-analysis-api")
 
-
 image = (
     modal.Image.debian_slim()
     .pip_install("aiohttp", "h2", "protobuf")
@@ -13,12 +12,11 @@ image = (
 
 @app.function(
     image=image,
-    keep_warm=1,
+    min_containers=1,   
     timeout=86400
 )
 @modal.web_server(port=3000)
 def start_web_service():
-    
     os.chdir("/root/app")
     os.environ["PORT"] = "3000"
     
